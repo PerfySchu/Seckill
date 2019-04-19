6e605f86fa96f87bdbfe1d26c5b8d1f904226836
PerfySchu <811601609@qq.com>
2019-04-19 13:23:46 +0800

    war package

--- src/main/java/com/perfyschu/seckill/MainApplication.java
+++ src/main/java/com/perfyschu/seckill/MainApplication.java
@@ -3,6 +3,8 @@ package com.perfyschu.seckill;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  * 文件名：Main.java
@@ -13,8 +15,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 @MapperScan(basePackages = {"com.perfyschu.seckill.dao"})
-public class MainApplication {
+public class MainApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(MainApplication.class);
     }
 }
